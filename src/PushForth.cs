5db7ec95f6c2bc006e67510a1863242d25eee7d5
Shane Celis <shane.celis@gmail.com>
2018-04-30 02:30:21 -0400

    Got something that's kind of working!

--- src/PushForth.cs
+++ src/PushForth.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using Sprache;
+using OneOf;
 
 namespace SeawispHunter.PushForth {
 
@@ -9,11 +11,33 @@ public interface Instruction {
   Stack Apply(Stack stack);
 }
 
+public class InstructionFunc : Instruction {
+  Func<Stack, Stack> func;
+  public InstructionFunc(Func<Stack, Stack> func) {
+    this.func = func;
+  }
+
+  public Stack Apply(Stack stack) {
+    return func(stack);
+  }
+}
+
 public class Symbol : Tuple<string> {
   public Symbol(string s) : base(s) { }
 }
 
+public class Cell : OneOfBase<Symbol, int, string> { }
+
 public class Interpreter {
+
+  public Dictionary<string, Instruction> instructions = new Dictionary<string, Instruction>();
+
+  public Interpreter() {
+    instructions["+"] = new InstructionFunc(stack => {
+        stack.Push((int) stack.Pop() + (int) stack.Pop());
+        return stack;
+      });
+  }
 
   private static readonly Parser<char> _quotedText =
     Parse.AnyChar.Except(Parse.Char('"'));
@@ -27,6 +51,7 @@ public class Interpreter {
     from open in Parse.Char('"')
     from text in escapedChar.Or(_quotedText).Many().Text()
     from close in Parse.Char('"')
+    from trailingSpaces in Parse.Char(' ').Many()
     select text;
 
   public static readonly Parser<string> bareWord = Parse.CharExcept(" []").AtLeastOnce().Text().Token();
@@ -49,20 +74,29 @@ public class Interpreter {
 
   private static readonly Parser<double> doubleRep =
     from op in Parse.Optional(Parse.Char('-').Token())
-    from num in Parse.Decimal
+    from num in Parse.Number
+    from dot in Parse.Char('.')
+    from frac in Parse.Number
     from trailingSpaces in Parse.Char(' ').Many()
-    select double.Parse(num) * (op.IsDefined ? -1 : 1);
+    select double.Parse(num + "." + frac) * (op.IsDefined ? -1 : 1);
 
-  private static readonly Parser<IOneOf<symbol, string, integer>> cell =
+  // private static readonly Parser<Cell> cell =
+  //   // quotedString.Or(bareWord).Or(integer.Select(i => i.ToString()));
+  //   quotedString.ToCell().Or(symbol.ToCell());
+
+  private static readonly Parser<object> cell =
     // quotedString.Or(bareWord).Or(integer.Select(i => i.ToString()));
-    quotedString.Or(symbol);
+    // quotedString.ToCell().Or(integer.ToCell()).Or(symbol.ToCell());
+    quotedString.ToCell().Or(floatRep.ToCell()).Or(doubleRep.ToCell()).Or(integer.ToCell()).Or(symbol.ToCell());
 
   private static readonly Parser<Stack> stackRep =
     from lbracket in Parse.Char('[')
-    // from contents in cell.Or(Parse.Ref(() => stackRep)).Many()
-    from contents in cell.Many()
+    from leadingSpaces in Parse.Char(' ').Many()
+    from contents in cell.Or(Parse.Ref(() => stackRep)).Many()
+    // from contents in cell.Many()
     from rbracket in Parse.Char(']')
-    select new Stack(contents.ToArray());
+    from trailingSpaces in Parse.Char(' ').Many()
+    select new Stack(contents.Reverse().ToArray());
 
   public static Stack Cons(object o, Stack stack) {
     stack.Push(o);
@@ -71,10 +105,9 @@ public class Interpreter {
 
   public static Stack ParseString(string s) {
     return stackRep.Parse(s);
-    // return null;
   }
 
-  public static Stack Eval(Stack stack) {
+  public Stack Eval(Stack stack) {
     if (! stack.Any())
       return stack; // halt
     object first = stack.Pop();
@@ -84,13 +117,21 @@ public class Interpreter {
       return Cons(code, stack);
     var data = stack;
     object obj = code.Pop();
+    Instruction ins;
+    if (obj is Symbol s) {
+      if (instructions.TryGetValue(s.Item1, out ins)) {
+        Console.WriteLine("Got an instruction!");
+        obj = ins;
+      }
+    }
     if (obj is Instruction) {
-      var ins = (Instruction) obj;
+      ins = (Instruction) obj;
       var result = ins.Apply(data);
+      Console.WriteLine("result " + string.Join(" ", result.ToArray()));
       if (! result.Any())
         return Cons(code, result);
 
-      object ret = result.Pop();
+      object ret = result.Peek();
       if (! (ret is Stack))
         return Cons(code, result);
       else if (((Stack) ret).Any())
@@ -108,6 +149,21 @@ public static class PushForthExtensions {
     return s.Count != 0;
   }
 
+  public static Parser<object> ToCell<T>(this Parser<T> parser) {
+    return parser.Select(t => (object) t);
+  }
+
+  // public static Parser<Cell> ToCell(this Parser<string> parser) {
+  //   return parser.Select(t => (Cell) t);
+  // }
+
+  // public static Parser<Cell> ToCell(this Parser<int> parser) {
+  //   return parser.Select(t => (Cell) t);
+  // }
+
+  // public static Parser<Cell> ToCell(this Parser<Symbol> parser) {
+  //   return parser.Select(t => (Cell) t);
+  // }
 }
 
 }
--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -7,24 +7,27 @@ namespace test
 {
 public class UnitTest1
 {
-  // [Fact]
+  [Fact]
   public void Test1()
   {
+  var interpreter = new Interpreter();
     var c = new Stack();
-    c.Push("+");
+    c.Push(new Symbol("+"));
     c.Push(1);
     c.Push(2);
-    var d = new Stack();
-    d.Push(c);
+    var d0 = new Stack();
+    d0.Push(c);
+    Assert.Equal(Interpreter.ParseString("[[2 1 +]]"), d0);
     var c_f = new Stack();
     var d_f = new Stack();
     d_f.Push(3);
     d_f.Push(c_f);
-    var d1 = Interpreter.Eval(d);
-    var d2 = Interpreter.Eval(d1);
-    var d3 = Interpreter.Eval(d2);
-
+    var d1 = interpreter.Eval(d0);
     Assert.Equal(Interpreter.ParseString("[[1 +] 2]"), d1);
+    var d2 = interpreter.Eval(d1);
+    Assert.Equal(Interpreter.ParseString("[[+] 1 2]"), d2);
+    var d3 = interpreter.Eval(d2);
+    Assert.Equal(Interpreter.ParseString("[[] 3]"), d3);
     Assert.Equal(d_f, d3);
   }
 
@@ -32,11 +35,37 @@ public class UnitTest1
   public void TestParsing() {
     var a = new Stack();
     Assert.Equal(a, Interpreter.ParseString("[]"));
-    a.Push("hi");
+    a.Push(new Symbol("hi"));
     Assert.Equal(a, Interpreter.ParseString("[hi]"));
-    a.Push("bye");
-    Assert.Equal(a, Interpreter.ParseString("[hi bye]"));
-    Assert.Equal(a, Interpreter.ParseString("[hi bye]"));
+    a.Push(new Symbol("bye"));
+    Assert.Equal(a, Interpreter.ParseString("[bye hi]"));
+    a.Push("string");
+    Assert.Equal(a, Interpreter.ParseString("[\"string\" bye hi]"));
+    Assert.Equal(a, Interpreter.ParseString("[\"string\"   bye   hi]"));
+    Assert.Equal(a, Interpreter.ParseString("[  \"string\"   bye   hi]"));
+    a.Push(1);
+    Assert.Equal(a, Interpreter.ParseString("[1  \"string\"   bye   hi]"));
+    a.Push(2f);
+    Assert.Equal(a, Interpreter.ParseString("[2f 1  \"string\"   bye   hi]"));
+    a.Push(3f);
+    Assert.Equal(a, Interpreter.ParseString("[3.0f  2f 1  \"string\"   bye   hi]"));
+    a.Push(4.0);
+    Assert.Equal(a, Interpreter.ParseString("[4.0 3.0f  2f 1  \"string\"   bye   hi]"));
+    a.Push(-4.0);
+    Assert.Equal(a, Interpreter.ParseString("[-4.0 4.0 3.0f  2f 1  \"string\"   bye   hi]"));
+  }
+
+  [Fact]
+  public void TestNestedParsing() {
+    var a = new Stack();
+    var b = new Stack();
+    a.Push(b);
+    Assert.Equal(a, Interpreter.ParseString("[[]]"));
+    b.Push(1);
+    Assert.Equal(a, Interpreter.ParseString("[[1]]"));
+    a.Push(2);
+    Assert.Equal(a, Interpreter.ParseString("[2[1]]"));
+    Assert.Equal(a, Interpreter.ParseString("[2 [1]]"));
   }
 }
 }
