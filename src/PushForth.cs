977d00f8b35f1a41a5555e3fc305fb0ecb6963a7
Shane Celis <shane.celis@gmail.com>
2018-05-02 04:42:01 -0400

    Added TrinaryFunc. Implemented i combinator and while.

--- src/PushForth.cs
+++ src/PushForth.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,8 +18,30 @@ public class InstructionFunc : Instruction {
     this.func = func;
   }
 
+  public InstructionFunc(Action<Stack> action)
+    : this(stack => { action(stack); return stack; }) {
+  }
+
   public Stack Apply(Stack stack) {
     return func(stack);
+  }
+}
+
+public class NoResultException : Exception { }
+
+public class NullaryFunc<X> : Instruction {
+  Func<X> func;
+  public NullaryFunc(Func<X> func) {
+    this.func = func;
+  }
+
+  public Stack Apply(Stack stack) {
+    try {
+      stack.Push(func());
+    } catch(NoResultException) {
+      // No-op.
+    }
+    return stack;
   }
 }
 
@@ -40,7 +63,11 @@ public class UnaryFunc<X, Y> : Instruction {
       stack.Push(code);
       return stack;
     }
-    stack.Push(func((X) a));
+    try {
+      stack.Push(func((X) a));
+    } catch(NoResultException) {
+      // No-op.
+    }
     return stack;
   }
 }
@@ -55,24 +82,79 @@ public class BinaryFunc<X, Y, Z> : Instruction {
     if (stack.Count < 2)
       return stack;
     object a, b;
-    b = stack.Pop();
-    if (! (b is Y)) {
-      var code = new Stack();
-      code.Push(b);
-      code.Push(this);
-      stack.Push(code);
-      return stack;
-    }
     a = stack.Pop();
     if (! (a is X)) {
       var code = new Stack();
       code.Push(a);
       code.Push(this);
-      stack.Push(b);
-      stack.Push(code);
+      stack.Push(new Continuation(code));
       return stack;
     }
-    stack.Push(func((X) a, (Y) b));
+    b = stack.Pop();
+    if (! (b is Y)) {
+      var code = new Stack();
+      code.Push(b);
+      code.Push(this);
+      stack.Push(a);
+      stack.Push(new Continuation(code));
+      return stack;
+    }
+    try {
+      stack.Push(func((X) a, (Y) b));
+    } catch(NoResultException) {
+      // No-op.
+    }
+    return stack;
+  }
+}
+
+public class TrinaryFunc<X, Y, Z> : Instruction {
+  Action<Stack, X, Y, Z> func;
+  public static TrinaryFunc<X, Y, Z> Func<W>(Func<X, Y, Z, W> func) {
+    return new TrinaryFunc<X, Y, Z>((stack, a, b, c) => {
+        try {
+          stack.Push(func(a, b, c));
+        } catch(NoResultException) {
+          // No-op.
+        }
+      });
+  }
+  public TrinaryFunc(Action<Stack, X, Y, Z> func) {
+    this.func = func;
+  }
+
+  public Stack Apply(Stack stack) {
+    if (stack.Count < 3)
+      return stack;
+    object a, b, c;
+    a = stack.Pop();
+    if (! (a is X)) {
+      var code = new Stack();
+      code.Push(a);
+      code.Push(this);
+      stack.Push(new Continuation(code));
+      return stack;
+    }
+    b = stack.Pop();
+    if (! (b is Y)) {
+      var code = new Stack();
+      code.Push(b);
+      code.Push(this);
+      stack.Push(a);
+      stack.Push(new Continuation(code));
+      return stack;
+    }
+    c = stack.Pop();
+    if (! (c is Z)) {
+      var code = new Stack();
+      code.Push(c);
+      code.Push(this);
+      stack.Push(b);
+      stack.Push(a);
+      stack.Push(new Continuation(code));
+      return stack;
+    }
+    func(stack, (X) a, (Y) b, (Z) c);
     return stack;
   }
 }
@@ -81,6 +163,13 @@ public class Symbol : Tuple<string> {
   public Symbol(string s) : base(s) { }
 
   public string name => Item1;
+  public override string ToString() => Item1;
+}
+
+public class Continuation : Tuple<Stack> {
+  public Continuation(Stack s) : base(s) { }
+
+  public Stack stack => Item1;
 }
 
 public class Cell : OneOfBase<Symbol, int, string> { }
@@ -90,8 +179,115 @@ public class Interpreter {
   public Dictionary<string, Instruction> instructions = new Dictionary<string, Instruction>();
 
   public Interpreter() {
+    instructions["i"] = new InstructionFunc(stack => {
+        if (stack.Any()) {
+          var x = stack.Pop();
+          Stack code;
+          if (x is Stack s) {
+            code = s;
+          } else {
+            // Bad argument.
+            code = new Stack();
+            code.Push(x);
+            code.Push(instructions["i"]);
+          }
+          stack.Push(new Continuation(code));
+        }
+      });
+    // instructions["i"] = new InstructionFunc(stack => {
+    //     if (stack.Any()) {
+    //       var code = new Stack();
+    //       code.Push(stack.Pop());
+    //       stack.Push(new Continuation(code));
+    //     }
+    //   });
+    instructions["car"] = new UnaryFunc<Stack, object>(stack => {
+        if (stack.Any())
+          return stack.Pop();
+        else
+          throw new NoResultException();
+      });
+    instructions["eval"]= new UnaryFunc<Stack, Stack>(stack => {
+        return Eval(stack);
+      });
+
+    // instructions["cdr"] = new InstructionFunc(stack => {
+    //     if (stack.Count < 1)
+    //       return stack;
+    //     object a;
+    //     a = stack.Pop();
+    //     Stack code;
+    //     if (! (a is Stack)) {
+    //       code = new Stack();
+    //       code.Push(a);
+    //       code.Push(this);
+    //       stack.Push(code);
+    //       return stack;
+    //     }
+    //     var s = (Stack) a;
+    //     s.Pop();
+    //     // Add a dummy code stack.
+    //     code = new Stack();
+    //     stack.Push(s);
+    //     stack.Push(code);
+    //     return stack;
+    //   });
+
+    // This looks like it's returning a code continuation.
+    instructions["cdr"] = new UnaryFunc<Stack, object>(stack => {
+        if (stack.Any())
+          stack.Pop();
+        return stack;
+      });
+    instructions["pop"] = new InstructionFunc(stack => {
+        if (stack.Any())
+          stack.Pop();
+      });
+    instructions["dup"] = new InstructionFunc(stack => {
+        if (stack.Any())
+          stack.Push(stack.Peek());
+      });
+    instructions["swap"] = new InstructionFunc(stack =>
+        {
+          if (stack.Count >= 2) {
+            var a = stack.Pop();
+            var b = stack.Pop();
+            stack.Push(a);
+            stack.Push(b);
+          }
+        });
+    instructions["cons"] = new BinaryFunc<object, Stack, Stack>((a, b) => Cons(a, b));
+    instructions["cat"] = new BinaryFunc<object, object, Stack>((a, b) =>
+        {
+          var s = new Stack();
+          s.Push(b);
+          s.Push(a);
+          return s;
+        });
+    instructions["split"] = new InstructionFunc(stack =>
+        {
+          if (stack.Any()) {
+            object o = stack.Pop();
+            if (o is Stack s) {
+              return Append(s, stack);
+            } else {
+              var code = new Stack();
+              code.Push(o);
+              code.Push(instructions["split"]);
+              stack.Push(new Continuation(code));
+            }
+          }
+          return stack;
+        });
+    instructions["unit"] = new UnaryFunc<object, Stack>(a => {
+        var s = new Stack();
+        s.Push(a);
+        return s;
+      });
+
     instructions["minus"] = new BinaryFunc<int, int, int>((a, b) => a - b);
     instructions["add"] = new BinaryFunc<int, int, int>((a, b) => a + b);
+    // InstructionFunc you have to do all your own error handling.
     instructions["+"] = new InstructionFunc(stack => {
         if (stack.Count < 2)
           return stack;
@@ -101,7 +297,7 @@ public class Interpreter {
           var code = new Stack();
           code.Push(a);
           code.Push(new Symbol("+"));
-          stack.Push(code);
+          stack.Push(new Continuation(code));
           return stack;
         }
         b = stack.Pop();
@@ -110,11 +306,51 @@ public class Interpreter {
           code.Push(b);
           code.Push(new Symbol("+"));
           stack.Push(a);
-          stack.Push(code);
+          stack.Push(new Continuation(code));
           return stack;
         }
         stack.Push((int) a + (int) b);
         return stack;
+      });
+    // instructions["while"] = new InstructionFunc(stack => {
+    //     if (stack.Count >= 3) {
+    //       object x = stack.Pop();
+    //       object y = stack.Pop();
+    //       object z = stack.Pop();
+    //       if (z is Stack Z && ! Z.Any()) {
+    //         stack.Push(y);
+    //       } else {
+    //         var code = new Stack();
+    //         // code.Push(instructions["i"]);
+    //         code.Push(new Symbol("i"));
+    //         var subcode = new Stack();
+    //         // subcode.Push(instructions["while"]);
+    //         subcode.Push(new Symbol("while"));
+    //         subcode.Push(x);
+    //         code.Push(subcode);
+    //         code.Push(x);
+    //         stack.Push(new Continuation(code));
+    //       }
+    //     }
+    //     return stack;
+    //   });
+    instructions["while"] = new TrinaryFunc<Stack, Stack, object>((stack, x, z, y) => {
+        if (! z.Any()) {
+          stack.Push(y);
+        } else {
+          var code = new Stack();
+          code.Push(instructions["i"]);
+          // code.Push(new Symbol("i"));
+          var subcode = new Stack();
+          subcode.Push(instructions["while"]);
+          // subcode.Push(new Symbol("while"));
+          subcode.Push(x);
+          code.Push(subcode);
+          code = Append(x, code);
+          // code.Push(x);
+          stack.Push(y);
+          stack.Push(new Continuation(code));
+        }
       });
   }
 
@@ -141,37 +377,65 @@ public class Interpreter {
     if (! stack.Any())
       return stack; // halt
     object first = stack.Pop();
-    Stack code = first as Stack;
-    if (! code.Any())
-      // Program is halted.
-      return Cons(code, stack);
-    var data = stack;
-    object obj = code.Pop();
-    Instruction ins;
-    if (obj is Symbol s) {
-      if (instructions.TryGetValue(s.Item1, out ins)) {
-        // Console.WriteLine("Got an instruction!");
-        obj = ins;
+    if (first is Stack code) {
+      if (! code.Any())
+        // Program is halted.
+        return Cons(code, stack);
+      var data = stack;
+      object obj = code.Pop();
+      Instruction ins;
+      if (obj is Symbol s) {
+        if (instructions.TryGetValue(s.Item1, out ins)) {
+          // Console.WriteLine("Got an instruction!");
+          obj = ins;
+        }
       }
-    }
-    if (obj is Instruction) {
-      ins = (Instruction) obj;
-      var result = ins.Apply(data);
-      // Console.WriteLine("result " + string.Join(" ", result.ToArray()));
-      if (! result.Any())
-        return Cons(code, result);
+      if (obj is Instruction) {
+        ins = (Instruction) obj;
+        var result = ins.Apply(data);
+        // Console.WriteLine("result " + string.Join(" ", result.ToArray()));
+        if (! result.Any())
+          return Cons(code, result);
 
-      object ret = result.Peek();
-      if (! (ret is Stack))
-        return Cons(code, result);
-      else if (((Stack) ret).Any())
-        code = Append((Stack)ret, code);
-      result.Pop();
-      data = result;
+        object ret = result.Peek();
+        if (! (ret is Continuation))
+          return Cons(code, result);
+        else
+          code = Append(((Continuation) ret).stack, code);
+        result.Pop();
+        data = result;
+      } else {
+        data = Cons(obj, data);
+      }
+      return Cons(code, data);
     } else {
-      data = Cons(obj, data);
+      return Cons(first, stack);
     }
-    return Cons(code, data);
+  }
+
+  public string StackToString(Stack s) {
+    var sb = new StringBuilder();
+    ToStringHelper(s, sb);
+    return sb.ToString();
+  }
+
+  void ToStringHelper(Stack s, StringBuilder sb) {
+    sb.Append("[");
+    var a = s.ToArray();
+    Array.Reverse(a);
+    s = new Stack(a);
+    while (s.Any()) {
+      object x = s.Pop();
+      if (x is Stack substack)
+        ToStringHelper(substack, sb);
+      else if (x is Instruction i)
+        sb.Append(instructions.First(kv => kv.Value == i).Key);
+      else
+        sb.Append(x.ToString());
+      if (s.Any())
+        sb.Append(" ");
+    }
+    sb.Append("]");
   }
 }
 
--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -136,7 +136,8 @@ public class UnitTest1
     // This is how Push3 does it.
     // (5 4 integer.-) => (1)
     // Not sure how I feel about it.  Feels weird.
-    Assert.Equal(Interpreter.ParseString("[[] 1]"), d3);
+    // I'm going to do it differently.
+    Assert.Equal(Interpreter.ParseString("[[] -1]"), d3);
   }
 
   [Fact]
@@ -185,6 +186,169 @@ public class UnitTest1
     a.Push(2);
     Assert.Equal(a, Interpreter.ParseString("[2[1]]"));
     Assert.Equal(a, Interpreter.ParseString("[2 [1]]"));
+  }
+
+  [Fact]
+  public void TestDup() {
+    var interpreter = new Interpreter();
+    var d0 = Interpreter.ParseString("[[dup] 5]");
+    var d1 = interpreter.Eval(d0);
+    Assert.Equal(Interpreter.ParseString("[[] 5 5]"), d1);
+  }
+
+  [Fact]
+  public void TestDupNoData() {
+    var interpreter = new Interpreter();
+    var d0 = Interpreter.ParseString("[[dup]]");
+    var d1 = interpreter.Eval(d0);
+    Assert.Equal(Interpreter.ParseString("[[]]"), d1);
+  }
+
+  [Fact]
+  public void TestSwap() {
+    var interpreter = new Interpreter();
+    var d0 = Interpreter.ParseString("[[swap] 1 2]");
+    var d1 = interpreter.Eval(d0);
+    Assert.Equal(Interpreter.ParseString("[[] 2 1]"), d1);
+  }
+
+  [Fact]
+  public void TestSwapLessData() {
+    var interpreter = new Interpreter();
+    var d0 = Interpreter.ParseString("[[swap] 2]");
+    var d1 = interpreter.Eval(d0);
+    Assert.Equal(Interpreter.ParseString("[[] 2]"), d1);
+  }
+
+  [Fact]
+  public void TestICombinatorWithBadArgument() {
+    var interpreter = new Interpreter();
+    var d0 = Interpreter.ParseString("[[i] 2]");
+    var d1 = interpreter.Eval(d0);
+    Assert.Equal(interpreter.ParseWithResolution("[[i 2]]"), d1);
+  }
+
+  [Fact]
+  public void TestICombinatorWithStack() {
+    var interpreter = new Interpreter();
+    var d0 = Interpreter.ParseString("[[i] [1 2 +]]");
+    var d1 = interpreter.Eval(d0);
+    // Assert.Equal(Interpreter.ParseString("[[1 2 +]]"), d1);
+    Assert.Equal("[[1 2 +]]", interpreter.StackToString(d1));
+    var d2 = interpreter.Eval(d1);
+    Assert.NotEqual(Interpreter.ParseString("[[] [1 2 +]]"), d2);
+    // Assert.Equal(Interpreter.ParseString("[[] 3]"), d2);
+  }
+
+  [Fact]
+  public void TestCar() {
+    var interpreter = new Interpreter();
+    var d0 = Interpreter.ParseString("[[car] [2]]");
+    var d1 = interpreter.Eval(d0);
+    Assert.Equal(Interpreter.ParseString("[[] 2]"), d1);
+  }
+
+  [Fact]
+  public void TestCdr() {
+    var interpreter = new Interpreter();
+    var d0 = Interpreter.ParseString("[[cdr] [2 5]]");
+    var d1 = interpreter.Eval(d0);
+    Assert.Equal(Interpreter.ParseString("[[] [5]]"), d1);
+  }
+
+  [Fact]
+  public void TestContinuationProblem() {
+    var interpreter = new Interpreter();
+    var d0 = Interpreter.ParseString("[[pop] 1 [2 5]]");
+    var d1 = interpreter.Eval(d0);
+    Assert.Equal(Interpreter.ParseString("[[] [2 5]]"), d1);
+  }
+
+  [Fact]
+  public void TestCat() {
+    var interpreter = new Interpreter();
+    var d0 = Interpreter.ParseString("[[cat] 1 2]]");
+    var d1 = interpreter.Eval(d0);
+    Assert.Equal(Interpreter.ParseString("[[] [1 2]]"), d1);
+  }
+
+  [Fact]
+  public void TestSplit() {
+    var interpreter = new Interpreter();
+    var d0 = Interpreter.ParseString("[[split] [1 2]]]");
+    var d1 = interpreter.Eval(d0);
+    Assert.Equal(Interpreter.ParseString("[[] 1 2]"), d1);
+  }
+
+  [Fact]
+  public void TestSplitMissingArg() {
+    var interpreter = new Interpreter();
+    var d0 = Interpreter.ParseString("[[split] 0 [1 2]]]");
+    var d1 = interpreter.Eval(d0);
+    var d2 = interpreter.Eval(d1);
+    var d3 = interpreter.Eval(d2);
+    Assert.Equal(Interpreter.ParseString("[[] 0 1 2]"), d3);
+  }
+
+  [Fact]
+  public void TestUnit() {
+    var interpreter = new Interpreter();
+    var d0 = Interpreter.ParseString("[[unit] 0 [1 2]]]");
+    var d1 = interpreter.Eval(d0);
+    Assert.Equal(Interpreter.ParseString("[[] [0] [1 2]]"), d1);
+  }
+
+  [Fact]
+  public void TestCons() {
+    var interpreter = new Interpreter();
+    var d0 = Interpreter.ParseString("[[cons] 0 [1 2]]]");
+    var d1 = interpreter.Eval(d0);
+    Assert.Equal(Interpreter.ParseString("[[] [0 1 2]]"), d1);
+  }
+
+  [Fact]
+  public void TestStackToString() {
+    var interpreter = new Interpreter();
+    var d0 = Interpreter.ParseString("[[cons] 0 [1 2]]]");
+    Assert.Equal("[[cons] 0 [1 2]]", interpreter.StackToString(d0));
+    Assert.Equal("[[cons] 0 [1 2]]", interpreter.StackToString(d0));
+  }
+
+  [Fact]
+  public void TestStackToString2() {
+    var interpreter = new Interpreter();
+    var d0 = interpreter.ParseWithResolution("[[cons] 0 [1 2]]]");
+    Assert.Equal("[[cons] 0 [1 2]]", interpreter.StackToString(d0));
+    Assert.Equal("[[cons] 0 [1 2]]", interpreter.StackToString(d0));
+  }
+
+  [Fact]
+  public void TestWhile() {
+    var interpreter = new Interpreter();
+    var d0 = Interpreter.ParseString("[[while] [1 +] [[]] 0]");
+    var d1 = interpreter.Eval(d0);
+    // Assert.Equal(Interpreter.ParseString("[[[1 +] [[1 +] while] i] 0]"), d1);
+    Assert.Equal("[[1 + [[1 +] while] i] 0]", interpreter.StackToString(d1));
+    var d2 = interpreter.Eval(d1);
+    Assert.Equal("[[+ [[1 +] while] i] 1 0]", interpreter.StackToString(d2));
+    var d3 = interpreter.Eval(d2);
+    Assert.Equal("[[[[1 +] while] i] 1]", interpreter.StackToString(d3));
+    var d4 = interpreter.Eval(d3);
+    Assert.Equal("[[i] [[1 +] while] 1]", interpreter.StackToString(d4));
+    var d5 = interpreter.Eval(d4);
+    Assert.Equal("[[[1 +] while] 1]", interpreter.StackToString(d5));
+    var d6 = interpreter.Eval(d5);
+    Assert.Equal("[[while] [1 +] 1]", interpreter.StackToString(d6));
+    var d7 = interpreter.Eval(d6);
+    Assert.Equal("[[] [1 +] 1]", interpreter.StackToString(d7));
+  }
+
+  [Fact]
+  public void TestEval() {
+    var interpreter = new Interpreter();
+    var d0 = Interpreter.ParseString("[[eval] [[+] 1 2]]");
+    var d1 = interpreter.Eval(d0);
+    Assert.Equal("[[] [[] 3]]", interpreter.StackToString(d1));
   }
 }
 }
